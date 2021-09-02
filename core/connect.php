f76dbcd0189aaa7d01269f39bfd5d14213631aae
Abragadabra <saychamini@gmail.com>
2021-09-02 08:47:54 +0500

    Подключаюсь к базе, используя класс PDO

---
+++ core/connect.php
@@ -0,0 +1,15 @@
+<?php
+$options = [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION];
+$driver = 'mysql';
+$charset = 'utf8';
+
+try {
+$pdo = new PDO("$driver:host=localhost;dbname=messanger;charser=$charset,
+    'root',
+    'root',
+    $options
+");
+} catch (PDOException $e) {
+    die('Ошибка при подключении к базе данных');
+}
+?>
