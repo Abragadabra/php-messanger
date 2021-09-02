fe5dc6b2d61ac47b852af477c27c1d3b6ef7c130
Abragadabra <saychamini@gmail.com>
2021-09-02 22:32:08 +0500

    Получаю данные в json массиве нормально

--- core/chat.php
+++ core/chat.php
@@ -14,12 +14,24 @@ if (!empty($message) && !empty($from)) {
     $result['send_status'] = $db -> query($sql);
 }
 
+
+//? Получаю сообщения из базы в json
+$start = isset($_GET['start']) ? intval($_GET['start']) : 0;
+
+//? Получаю данные из базы запросом
+$items = $db -> query("SELECT * FROM `messages` WHERE `id` >" . $start);
+
+//? Прохожусь циклом и помещаю в массив
+while($row = $items -> fetch_assoc()) {
+    $result['items'][] = $row;
+}
+
 //? Закрываю подключание к базе
 $db -> close();
 
 header('Access-Control-Allow-Orgin: *');
 header('Content-Type: application/json');
 
-//? Помещаю в json 
+//? Превращаю в json 
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
 ?>
