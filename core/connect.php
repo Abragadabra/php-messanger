ade938ddef64d6029f32aa647a2cbbfc20a4e328
Abragadabra <saychamini@gmail.com>
2021-09-02 08:51:43 +0500

    Исправил подключание к базе

--- core/connect.php
+++ core/connect.php
@@ -4,11 +4,11 @@ $driver = 'mysql';
 $charset = 'utf8';
 
 try {
-$pdo = new PDO("$driver:host=localhost;dbname=messanger;charser=$charset,
+$pdo = new PDO("$driver:host=localhost;dbname=messanger;charset=$charset",
     'root',
     'root',
     $options
-");
+);
 } catch (PDOException $e) {
     die('Ошибка при подключении к базе данных');
 }
