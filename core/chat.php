f03740e2feb1d6a4a237a68296b9d817342f0c28
Abragadabra <saychamini@gmail.com>
2021-09-02 22:18:45 +0500

    Комментарии в chat.php

--- core/chat.php
+++ core/chat.php
@@ -7,14 +7,19 @@ $message = isset($_POST['message']) ? $_POST['message'] : null;
 $from = isset($_POST['from']) ? $_POST['from'] : null;
 
 if (!empty($message) && !empty($from)) {
+    //? Запрос к БД
     $sql = "INSERT INTO `messages` (`message`, `username`) VALUES ('".$message."', '".$from."')";
+
+    //? Помещаю реузльтат выполнения запроса к базе в result
     $result['send_status'] = $db -> query($sql);
 }
 
+//? Закрываю подключание к базе
 $db -> close();
 
 header('Access-Control-Allow-Orgin: *');
 header('Content-Type: application/json');
 
-echo json_encode($result);
+//? Помещаю в json 
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
 ?>
