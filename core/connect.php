b367bc8acfd8e09d63b6f788e181145dbd198e10
Abragadabra <saychamini@gmail.com>
2021-09-02 11:55:36 +0500

    Начинаю переписывать

--- core/connect.php
+++ core/connect.php
@@ -1,15 +1,3 @@
 <?php
-$options = [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION];
-$driver = 'mysql';
-$charset = 'utf8';
 
-try {
-$pdo = new PDO("$driver:host=localhost;dbname=messanger;charset=$charset",
-    'root',
-    'root',
-    $options
-);
-} catch (PDOException $e) {
-    die('Ошибка при подключении к базе данных');
-}
 ?>
