484ff32b91708f4a532e80b7f51044b0e5b41cd3
Abragadabra <saychamini@gmail.com>
2021-09-02 22:14:15 +0500

    Доделал подключание к базе, в chat.php получаю сообщения и помещаю в базу с помощью mysqli

--- core/chat.php
+++ core/chat.php
@@ -1,3 +1,20 @@
 <?php
 require_once 'connect.php';
+
+$result = array();
+
+$message = isset($_POST['message']) ? $_POST['message'] : null;
+$from = isset($_POST['from']) ? $_POST['from'] : null;
+
+if (!empty($message) && !empty($from)) {
+    $sql = "INSERT INTO `messages` (`message`, `username`) VALUES ('".$message."', '".$from."')";
+    $result['send_status'] = $db -> query($sql);
+}
+
+$db -> close();
+
+header('Access-Control-Allow-Orgin: *');
+header('Content-Type: application/json');
+
+echo json_encode($result);
 ?>
--- core/connect.php
+++ core/connect.php
@@ -1,3 +1,7 @@
 <?php
+$db = new mysqli('localhost', 'root', 'root', 'messanger');
 
+if ($db -> connect_error) {
+    die('Ошибка подключения' . $db -> connect_error);
+}
 ?>
